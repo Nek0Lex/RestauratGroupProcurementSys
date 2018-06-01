f011688a67f53e3e50c282388ec27b046720948c
YEUNG Yat Hei <michael99yeung@gmail.com>
2018-06-02 00:09:10 +0800

    designed header of the form which shows the detail of restaurant and staff

---
+++ ProcurementSystem/ProcurementSystem/Create Purchase Request.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace ProcurementSystem
+{
+    partial class Create_Purchase_Order
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.title = new System.Windows.Forms.Label();
+            this.txtRestName = new System.Windows.Forms.Label();
+            this.txtRestId = new System.Windows.Forms.Label();
+            this.txtStfName = new System.Windows.Forms.Label();
+            this.txtStaffId = new System.Windows.Forms.Label();
+            this.RestName = new System.Windows.Forms.Label();
+            this.RestId = new System.Windows.Forms.Label();
+            this.StfId = new System.Windows.Forms.Label();
+            this.StfName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.Font = new System.Drawing.Font("新細明體", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.title.Location = new System.Drawing.Point(185, 27);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(385, 37);
+            this.title.TabIndex = 0;
+            this.title.Text = "Create Purchase Request";
+            // 
+            // txtRestName
+            // 
+            this.txtRestName.AutoSize = true;
+            this.txtRestName.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtRestName.Location = new System.Drawing.Point(137, 81);
+            this.txtRestName.Name = "txtRestName";
+            this.txtRestName.Size = new System.Drawing.Size(161, 19);
+            this.txtRestName.TabIndex = 1;
+            this.txtRestName.Text = "Restaurant Name :";
+            // 
+            // txtRestId
+            // 
+            this.txtRestId.AutoSize = true;
+            this.txtRestId.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtRestId.Location = new System.Drawing.Point(137, 100);
+            this.txtRestId.Name = "txtRestId";
+            this.txtRestId.Size = new System.Drawing.Size(135, 19);
+            this.txtRestId.TabIndex = 2;
+            this.txtRestId.Text = "Restaurant ID :";
+            this.txtRestId.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtStfName
+            // 
+            this.txtStfName.AutoSize = true;
+            this.txtStfName.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtStfName.Location = new System.Drawing.Point(479, 81);
+            this.txtStfName.Name = "txtStfName";
+            this.txtStfName.Size = new System.Drawing.Size(114, 19);
+            this.txtStfName.TabIndex = 3;
+            this.txtStfName.Text = "Staff Name :";
+            // 
+            // txtStaffId
+            // 
+            this.txtStaffId.AutoSize = true;
+            this.txtStaffId.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtStaffId.Location = new System.Drawing.Point(479, 100);
+            this.txtStaffId.Name = "txtStaffId";
+            this.txtStaffId.Size = new System.Drawing.Size(88, 19);
+            this.txtStaffId.TabIndex = 4;
+            this.txtStaffId.Text = "Staff ID :";
+            // 
+            // RestName
+            // 
+            this.RestName.AutoSize = true;
+            this.RestName.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Bold);
+            this.RestName.Location = new System.Drawing.Point(304, 81);
+            this.RestName.Name = "RestName";
+            this.RestName.Size = new System.Drawing.Size(94, 19);
+            this.RestName.TabIndex = 5;
+            this.RestName.Text = "//restname";
+            // 
+            // RestId
+            // 
+            this.RestId.AutoSize = true;
+            this.RestId.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Bold);
+            this.RestId.Location = new System.Drawing.Point(278, 100);
+            this.RestId.Name = "RestId";
+            this.RestId.Size = new System.Drawing.Size(72, 19);
+            this.RestId.TabIndex = 6;
+            this.RestId.Text = "//restID";
+            // 
+            // StfId
+            // 
+            this.StfId.AutoSize = true;
+            this.StfId.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Bold);
+            this.StfId.Location = new System.Drawing.Point(573, 100);
+            this.StfId.Name = "StfId";
+            this.StfId.Size = new System.Drawing.Size(79, 19);
+            this.StfId.TabIndex = 7;
+            this.StfId.Text = "//staffID";
+            // 
+            // StfName
+            // 
+            this.StfName.AutoSize = true;
+            this.StfName.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Bold);
+            this.StfName.Location = new System.Drawing.Point(599, 81);
+            this.StfName.Name = "StfName";
+            this.StfName.Size = new System.Drawing.Size(101, 19);
+            this.StfName.TabIndex = 8;
+            this.StfName.Text = "//staffname";
+            // 
+            // Create_Purchase_Order
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.StfName);
+            this.Controls.Add(this.StfId);
+            this.Controls.Add(this.RestId);
+            this.Controls.Add(this.RestName);
+            this.Controls.Add(this.txtStaffId);
+            this.Controls.Add(this.txtStfName);
+            this.Controls.Add(this.txtRestId);
+            this.Controls.Add(this.txtRestName);
+            this.Controls.Add(this.title);
+            this.Name = "Create_Purchase_Order";
+            this.Text = "Create_Purchase_Request";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Label txtRestName;
+        private System.Windows.Forms.Label txtRestId;
+        private System.Windows.Forms.Label txtStfName;
+        private System.Windows.Forms.Label txtStaffId;
+        private System.Windows.Forms.Label RestName;
+        private System.Windows.Forms.Label RestId;
+        private System.Windows.Forms.Label StfId;
+        private System.Windows.Forms.Label StfName;
+    }
+}
---
+++ ProcurementSystem/ProcurementSystem/Create Purchase Request.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProcurementSystem
+{
+    public partial class Create_Purchase_Order : Form
+    {
+        public Create_Purchase_Order()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
