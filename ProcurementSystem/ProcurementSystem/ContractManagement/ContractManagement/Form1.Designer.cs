365b2329c49036d4e6cb8b2912cac4ab866b8af4
AlexWong <wonghoming111105@gmail.com>
2018-06-02 14:31:41 +0800

    Created Blanket Purchase Agreement UI

--- ProcurementSystem/ProcurementSystem/ContractManagement/ContractManagement/Form1.Designer.cs
+++ ProcurementSystem/ProcurementSystem/ContractManagement/ContractManagement/Form1.Designer.cs
@@ -28,20 +28,120 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("微軟正黑體", 20F, System.Drawing.FontStyle.Bold);
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(776, 46);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Blanket Purchase Agreement";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkedListBox1);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 58);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(776, 380);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(577, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(181, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Create New Blanket Agreement";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(577, 92);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(181, 31);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Edit Blanket Agreement";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(577, 158);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(181, 35);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Delete Selected Blanket Agreement";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(577, 342);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(181, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "back";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.checkedListBox1.Font = new System.Drawing.Font("新細明體", 15F);
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "BlanketAgreementList1",
+            "BlanketAgreementList2",
+            "BlanketAgreementList3"});
+            this.checkedListBox1.Location = new System.Drawing.Point(17, 21);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.ScrollAlwaysVisible = true;
+            this.checkedListBox1.Size = new System.Drawing.Size(541, 342);
+            this.checkedListBox1.TabIndex = 5;
+            this.checkedListBox1.ThreeDCheckBoxes = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBox1);
             this.Name = "Form1";
-            this.Text = "Create Contract";
+            this.Text = "Blanket Purchase Agreements";
+            this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        public System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- ProcurementSystem/ProcurementSystem/ContractManagement/ContractManagement/Form1.cs
+++ ProcurementSystem/ProcurementSystem/ContractManagement/ContractManagement/Form1.cs
@@ -16,5 +16,20 @@ namespace ContractManagement
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
