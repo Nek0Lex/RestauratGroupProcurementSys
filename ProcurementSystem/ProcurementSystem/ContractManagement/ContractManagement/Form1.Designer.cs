29d74cac62b786754dee2f1018b83a0d39b535d7
AlexWong <wonghoming111105@gmail.com>
2018-06-02 14:58:55 +0800

    Created Blanket Purchase Argeement UI

--- ProcurementSystem/ProcurementSystem/ContractManagement/ContractManagement/Form1.Designer.cs
+++ ProcurementSystem/ProcurementSystem/ContractManagement/ContractManagement/Form1.Designer.cs
@@ -28,13 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem(new string[] {
+            "Agreement 1",
+            "dd/mm/yyyy"}, -1);
+            System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem("Agreement 2");
+            System.Windows.Forms.ListViewItem listViewItem3 = new System.Windows.Forms.ListViewItem("Agreement 3");
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
-            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -52,7 +60,7 @@
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.checkedListBox1);
+            this.groupBox1.Controls.Add(this.listView1);
             this.groupBox1.Controls.Add(this.button4);
             this.groupBox1.Controls.Add(this.button3);
             this.groupBox1.Controls.Add(this.button2);
@@ -66,9 +74,10 @@
             // 
             // button1
             // 
+            this.button1.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.button1.Location = new System.Drawing.Point(577, 21);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(181, 34);
+            this.button1.Size = new System.Drawing.Size(181, 27);
             this.button1.TabIndex = 1;
             this.button1.Text = "Create New Blanket Agreement";
             this.button1.UseVisualStyleBackColor = true;
@@ -76,18 +85,20 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(577, 92);
+            this.button2.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button2.Location = new System.Drawing.Point(577, 83);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(181, 31);
+            this.button2.Size = new System.Drawing.Size(181, 27);
             this.button2.TabIndex = 2;
             this.button2.Text = "Edit Blanket Agreement";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(577, 158);
+            this.button3.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button3.Location = new System.Drawing.Point(577, 147);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(181, 35);
+            this.button3.Size = new System.Drawing.Size(181, 27);
             this.button3.TabIndex = 3;
             this.button3.Text = "Delete Selected Blanket Agreement";
             this.button3.UseVisualStyleBackColor = true;
@@ -101,21 +112,37 @@
             this.button4.Text = "back";
             this.button4.UseVisualStyleBackColor = true;
             // 
-            // checkedListBox1
+            // listView1
             // 
-            this.checkedListBox1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.checkedListBox1.Font = new System.Drawing.Font("新細明體", 15F);
-            this.checkedListBox1.FormattingEnabled = true;
-            this.checkedListBox1.Items.AddRange(new object[] {
-            "BlanketAgreementList1",
-            "BlanketAgreementList2",
-            "BlanketAgreementList3"});
-            this.checkedListBox1.Location = new System.Drawing.Point(17, 21);
-            this.checkedListBox1.Name = "checkedListBox1";
-            this.checkedListBox1.ScrollAlwaysVisible = true;
-            this.checkedListBox1.Size = new System.Drawing.Size(541, 342);
-            this.checkedListBox1.TabIndex = 5;
-            this.checkedListBox1.ThreeDCheckBoxes = true;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.GridLines = true;
+            this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1,
+            listViewItem2,
+            listViewItem3});
+            this.listView1.Location = new System.Drawing.Point(19, 22);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(521, 343);
+            this.listView1.TabIndex = 5;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Agreement Name";
+            this.columnHeader1.Width = 350;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Date";
+            this.columnHeader2.Width = 80;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "option";
             // 
             // Form1
             // 
@@ -140,8 +167,11 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
-        public System.Windows.Forms.CheckedListBox checkedListBox1;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
     }
 }
 
