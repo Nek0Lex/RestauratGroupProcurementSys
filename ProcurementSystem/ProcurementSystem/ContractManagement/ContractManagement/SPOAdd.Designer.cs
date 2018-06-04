f4437c33bc902f334fe9eeefe428fcaa5978f8d4
AlexWong <wonghoming111105@gmail.com>
2018-06-04 11:07:28 +0800

    Updated SPO add UI

--- ProcurementSystem/ProcurementSystem/ContractManagement/ContractManagement/SPOAdd.Designer.cs
+++ ProcurementSystem/ProcurementSystem/ContractManagement/ContractManagement/SPOAdd.Designer.cs
@@ -61,10 +61,11 @@
             this.textBox1.BackColor = System.Drawing.SystemColors.Window;
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox1.Font = new System.Drawing.Font("微軟正黑體", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Location = new System.Drawing.Point(14, 16);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(776, 36);
+            this.textBox1.Size = new System.Drawing.Size(905, 36);
             this.textBox1.TabIndex = 1;
             this.textBox1.Text = "New Stanard Purchase Order";
             // 
@@ -90,9 +91,11 @@
             this.groupBox1.Controls.Add(this.textBox4);
             this.groupBox1.Controls.Add(this.textBox3);
             this.groupBox1.Controls.Add(this.textBox2);
-            this.groupBox1.Location = new System.Drawing.Point(12, 54);
+            this.groupBox1.Location = new System.Drawing.Point(14, 72);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(816, 419);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox1.Size = new System.Drawing.Size(952, 558);
             this.groupBox1.TabIndex = 2;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Create new SPO";
@@ -100,19 +103,21 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(81, 41);
+            this.textBox2.Location = new System.Drawing.Point(94, 54);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.Size = new System.Drawing.Size(116, 23);
             this.textBox2.TabIndex = 0;
             // 
             // textBox3
             // 
             this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox3.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.textBox3.Location = new System.Drawing.Point(25, 45);
+            this.textBox3.Location = new System.Drawing.Point(29, 60);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox3.Name = "textBox3";
             this.textBox3.ReadOnly = true;
-            this.textBox3.Size = new System.Drawing.Size(50, 18);
+            this.textBox3.Size = new System.Drawing.Size(58, 18);
             this.textBox3.TabIndex = 1;
             this.textBox3.Text = "SPONo:";
             // 
@@ -120,64 +125,71 @@
             // 
             this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox4.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.textBox4.Location = new System.Drawing.Point(25, 95);
+            this.textBox4.Location = new System.Drawing.Point(29, 126);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox4.Name = "textBox4";
             this.textBox4.ReadOnly = true;
-            this.textBox4.Size = new System.Drawing.Size(80, 18);
+            this.textBox4.Size = new System.Drawing.Size(93, 18);
             this.textBox4.TabIndex = 2;
             this.textBox4.Text = "SupplierNo:";
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(111, 91);
+            this.textBox5.Location = new System.Drawing.Point(129, 122);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(100, 22);
+            this.textBox5.Size = new System.Drawing.Size(116, 23);
             this.textBox5.TabIndex = 3;
             // 
             // textBox7
             // 
             this.textBox7.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox7.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.textBox7.Location = new System.Drawing.Point(25, 149);
+            this.textBox7.Location = new System.Drawing.Point(29, 198);
+            this.textBox7.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox7.Name = "textBox7";
             this.textBox7.ReadOnly = true;
-            this.textBox7.Size = new System.Drawing.Size(94, 18);
+            this.textBox7.Size = new System.Drawing.Size(110, 18);
             this.textBox7.TabIndex = 5;
             this.textBox7.Text = "Creation Date:";
             // 
             // dateTimePicker1
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(125, 145);
+            this.dateTimePicker1.Location = new System.Drawing.Point(146, 194);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(114, 22);
+            this.dateTimePicker1.Size = new System.Drawing.Size(132, 23);
             this.dateTimePicker1.TabIndex = 6;
             // 
             // textBox6
             // 
             this.textBox6.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox6.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.textBox6.Location = new System.Drawing.Point(25, 201);
+            this.textBox6.Location = new System.Drawing.Point(29, 268);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox6.Name = "textBox6";
             this.textBox6.ReadOnly = true;
-            this.textBox6.Size = new System.Drawing.Size(94, 18);
+            this.textBox6.Size = new System.Drawing.Size(110, 18);
             this.textBox6.TabIndex = 7;
             this.textBox6.Text = "Effective Date:";
             // 
             // dateTimePicker2
             // 
-            this.dateTimePicker2.Location = new System.Drawing.Point(125, 197);
+            this.dateTimePicker2.Location = new System.Drawing.Point(146, 262);
+            this.dateTimePicker2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.dateTimePicker2.Name = "dateTimePicker2";
-            this.dateTimePicker2.Size = new System.Drawing.Size(114, 22);
+            this.dateTimePicker2.Size = new System.Drawing.Size(132, 23);
             this.dateTimePicker2.TabIndex = 8;
             // 
             // textBox8
             // 
             this.textBox8.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox8.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.textBox8.Location = new System.Drawing.Point(345, 201);
+            this.textBox8.Location = new System.Drawing.Point(402, 268);
+            this.textBox8.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox8.Name = "textBox8";
             this.textBox8.ReadOnly = true;
-            this.textBox8.Size = new System.Drawing.Size(87, 18);
+            this.textBox8.Size = new System.Drawing.Size(101, 18);
             this.textBox8.TabIndex = 9;
             this.textBox8.Text = "Buyer Name:";
             // 
@@ -185,25 +197,28 @@
             // 
             this.textBox9.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox9.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.textBox9.Location = new System.Drawing.Point(345, 41);
+            this.textBox9.Location = new System.Drawing.Point(402, 54);
+            this.textBox9.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox9.Name = "textBox9";
             this.textBox9.ReadOnly = true;
-            this.textBox9.Size = new System.Drawing.Size(97, 18);
+            this.textBox9.Size = new System.Drawing.Size(113, 18);
             this.textBox9.TabIndex = 10;
             this.textBox9.Text = "Billing Address:";
             // 
             // textBox10
             // 
-            this.textBox10.Location = new System.Drawing.Point(438, 197);
+            this.textBox10.Location = new System.Drawing.Point(511, 262);
+            this.textBox10.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox10.Name = "textBox10";
-            this.textBox10.Size = new System.Drawing.Size(135, 22);
+            this.textBox10.Size = new System.Drawing.Size(157, 23);
             this.textBox10.TabIndex = 11;
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(448, 41);
+            this.richTextBox1.Location = new System.Drawing.Point(523, 54);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(293, 102);
+            this.richTextBox1.Size = new System.Drawing.Size(341, 134);
             this.richTextBox1.TabIndex = 14;
             this.richTextBox1.Text = "";
             // 
@@ -211,115 +226,132 @@
             // 
             this.textBox11.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox11.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.textBox11.Location = new System.Drawing.Point(345, 158);
+            this.textBox11.Location = new System.Drawing.Point(402, 210);
+            this.textBox11.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox11.Name = "textBox11";
             this.textBox11.ReadOnly = true;
-            this.textBox11.Size = new System.Drawing.Size(97, 18);
+            this.textBox11.Size = new System.Drawing.Size(113, 18);
             this.textBox11.TabIndex = 15;
             this.textBox11.Text = "Buyer Account:";
             // 
             // textBox12
             // 
-            this.textBox12.Location = new System.Drawing.Point(448, 158);
+            this.textBox12.Location = new System.Drawing.Point(523, 210);
+            this.textBox12.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox12.Name = "textBox12";
-            this.textBox12.Size = new System.Drawing.Size(142, 22);
+            this.textBox12.Size = new System.Drawing.Size(165, 23);
             this.textBox12.TabIndex = 16;
             // 
             // textBox13
             // 
             this.textBox13.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox13.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.textBox13.Location = new System.Drawing.Point(345, 250);
+            this.textBox13.Location = new System.Drawing.Point(402, 334);
+            this.textBox13.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox13.Name = "textBox13";
             this.textBox13.ReadOnly = true;
-            this.textBox13.Size = new System.Drawing.Size(54, 18);
+            this.textBox13.Size = new System.Drawing.Size(63, 18);
             this.textBox13.TabIndex = 17;
             this.textBox13.Text = "RestNo:";
             // 
             // textBox14
             // 
-            this.textBox14.Location = new System.Drawing.Point(405, 246);
+            this.textBox14.Location = new System.Drawing.Point(472, 328);
+            this.textBox14.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox14.Name = "textBox14";
-            this.textBox14.Size = new System.Drawing.Size(142, 22);
+            this.textBox14.Size = new System.Drawing.Size(165, 23);
             this.textBox14.TabIndex = 18;
             // 
             // textBox15
             // 
             this.textBox15.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox15.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.textBox15.Location = new System.Drawing.Point(25, 250);
+            this.textBox15.Location = new System.Drawing.Point(29, 334);
+            this.textBox15.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox15.Name = "textBox15";
             this.textBox15.ReadOnly = true;
-            this.textBox15.Size = new System.Drawing.Size(146, 18);
+            this.textBox15.Size = new System.Drawing.Size(170, 18);
             this.textBox15.TabIndex = 19;
             this.textBox15.Text = "Expected Delivery Date:";
             // 
             // dateTimePicker3
             // 
-            this.dateTimePicker3.Location = new System.Drawing.Point(177, 250);
+            this.dateTimePicker3.Location = new System.Drawing.Point(206, 334);
+            this.dateTimePicker3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.dateTimePicker3.Name = "dateTimePicker3";
-            this.dateTimePicker3.Size = new System.Drawing.Size(114, 22);
+            this.dateTimePicker3.Size = new System.Drawing.Size(132, 23);
             this.dateTimePicker3.TabIndex = 20;
             // 
             // textBox16
             // 
             this.textBox16.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox16.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.textBox16.Location = new System.Drawing.Point(345, 294);
+            this.textBox16.Location = new System.Drawing.Point(402, 392);
+            this.textBox16.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox16.Name = "textBox16";
             this.textBox16.ReadOnly = true;
-            this.textBox16.Size = new System.Drawing.Size(135, 18);
+            this.textBox16.Size = new System.Drawing.Size(157, 18);
             this.textBox16.TabIndex = 21;
             this.textBox16.Text = "Terms And Condition:";
             this.textBox16.TextChanged += new System.EventHandler(this.textBox16_TextChanged);
             // 
             // richTextBox2
             // 
-            this.richTextBox2.Location = new System.Drawing.Point(486, 293);
+            this.richTextBox2.Location = new System.Drawing.Point(567, 390);
+            this.richTextBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(293, 102);
+            this.richTextBox2.Size = new System.Drawing.Size(341, 134);
             this.richTextBox2.TabIndex = 22;
             this.richTextBox2.Text = "";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(83, 503);
+            this.button1.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.Location = new System.Drawing.Point(97, 670);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(149, 44);
+            this.button1.Size = new System.Drawing.Size(174, 58);
             this.button1.TabIndex = 3;
-            this.button1.Text = "button1";
+            this.button1.Text = "<Back";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(328, 503);
+            this.button2.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button2.Location = new System.Drawing.Point(383, 670);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(149, 44);
+            this.button2.Size = new System.Drawing.Size(174, 58);
             this.button2.TabIndex = 4;
-            this.button2.Text = "button2";
+            this.button2.Text = "Clear";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(556, 503);
+            this.button3.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button3.Location = new System.Drawing.Point(649, 670);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(149, 44);
+            this.button3.Size = new System.Drawing.Size(174, 58);
             this.button3.TabIndex = 5;
-            this.button3.Text = "button3";
+            this.button3.Text = "Submit";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // SPOAdd
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
             this.BackColor = System.Drawing.SystemColors.Window;
-            this.ClientSize = new System.Drawing.Size(841, 575);
+            this.ClientSize = new System.Drawing.Size(992, 758);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("微軟正黑體", 9F);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "SPOAdd";
             this.Text = "SPOAdd";
             this.Load += new System.EventHandler(this.SPUAdd_Load);
--- ProcurementSystem/ProcurementSystem/ContractManagement/ContractManagement/SPOAdd.cs
+++ ProcurementSystem/ProcurementSystem/ContractManagement/ContractManagement/SPOAdd.cs
@@ -31,5 +31,10 @@ namespace ContractManagement
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
