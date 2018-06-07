5b6f3b86009c4f4330c94c0cb2bc1000b90ac057
Code4Cat <tongkaitik1997@gmail.com>
2018-06-07 21:50:58 +0800

    Add some code

--- ProcurementSystem/ProcurementSystem/Category.cs
+++ ProcurementSystem/ProcurementSystem/Category.cs
@@ -16,6 +16,7 @@ namespace ProcurementSystem
     {
         MySqlConnection cnn = new MySqlConnection("server=code4cat.me;user id=jackysc;password=123456;database=procurement;SslMode=none;");
         private Menu m = null;
+        //List<object> cat;
 
         public Category(Menu m)
         {
@@ -23,14 +24,56 @@ namespace ProcurementSystem
             this.m = m;
         }
 
-        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
-        {
-
-        }
-
         private void Category_Load(object sender, EventArgs e)
         {
+            cnn.Open();
+            MySqlDataAdapter sda = new MySqlDataAdapter("SELECT * FROM Category;", cnn);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            //cat = dt.AsEnumerable().ToList<object>();
+            //treeView1.DataSource = sda;
+            foreach (DataRow dr in dt.Rows)
+            {
+                if (dr["parent_id"].ToString().Equals(""))
+                {
+                    TreeNode treeNode = new TreeNode(dr["name"].ToString());
+                    treeNode.Tag = dr["category_id"].ToString();
+                    treeView1.Nodes.Add(treeNode);
 
+                    MySqlDataAdapter sdaChild = new MySqlDataAdapter("SELECT* FROM Category WHERE parent_id = " + dr["category_id"].ToString() + ";", cnn);
+                    DataTable dtChild = new DataTable();
+                    sdaChild.Fill(dtChild);
+                    foreach (DataRow drChild in dtChild.Rows)
+                    {
+                        TreeNode childNode = new TreeNode(drChild["name"].ToString());
+                        childNode.Tag = drChild["category_id"].ToString();
+                        treeNode.Nodes.Add(childNode);
+                    }
+
+                }
+            }
+            cnn.Close();
         }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            cnn.Open();
+            MySqlDataAdapter sda = new MySqlDataAdapter("SELECT VItemID, i.ItemID, ItemName,ItemDescription FROM VItem v, Item i where v.ItemId=i.ItemID AND v.category_id = " + treeView1.SelectedNode.Tag.ToString() + "; ", cnn);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            dataGridView1.DataSource = dt;
+            cnn.Close();
+        }
+
+        private void AddCategory_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show(treeView1.SelectedNode.Tag.ToString());
+            //string categoryName;
+            //MySqlCommand add = new MySqlCommand("INSERT INTO Category (name, parent_id) Value (" + categoryName + ", " + treeView1.SelectedNode.Tag.ToString() + ");", cnn);
+            //add.ExecuteNonQuery();
+        }
+        
     }
+
+
 }
