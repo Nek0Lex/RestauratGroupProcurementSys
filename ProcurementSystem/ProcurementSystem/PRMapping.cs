18a8efe0fc893e3c9edb99718cd58172acfea7a0
YEUNG Yat Hei <michael99yeung@gmail.com>
2018-06-26 20:57:17 +0800

    little change on prmap

--- ProcurementSystem/ProcurementSystem/PRMapping.cs
+++ ProcurementSystem/ProcurementSystem/PRMapping.cs
@@ -7,14 +7,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace ProcurementSystem
 {
     public partial class PRMapping : Form
     {
+        MySqlConnection cnn = new MySqlConnection("server=code4cat.me;user id=jackysc;password=123456;database=procurement;SslMode=none;");
         public PRMapping()
         {
             InitializeComponent();
+            cnn.Open();
+            MySqlDataAdapter findRequestList = new MySqlDataAdapter("Select RequestNo, CreationDate From PurchaseRequest where status='NEW';", cnn);
+            DataTable dt = new DataTable();
+            findRequestList.Fill(dt);
+            foreach (DataRow dr in dt.Rows) {
+                requestList.Rows.Add(dr["RequestNo"].ToString(), dr["CreationDate"].ToString());
+            }
         }
     }
 }
