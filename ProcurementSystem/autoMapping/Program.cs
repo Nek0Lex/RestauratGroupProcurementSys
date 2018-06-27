f7c6fa2f3795b645e8d669c6904b27d43fdf5097
YEUNG Yat Hei <michael99yeung@gmail.com>
2018-06-27 13:50:00 +0800

    test

---
+++ ProcurementSystem/autoMapping/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace autoMapping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
